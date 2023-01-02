6ec21521b4c2f74ffdd7d1d608dcea7e17804658
mattyydagz <mnjdagostino@gamil.com>
2023-01-02 13:02:02 -0500

    Edits to header and Nav dots

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -3,19 +3,24 @@ import React from "react";
 const NavigationDots = ({ active }) => {
   return (
     <div className="app__navigation">
-      {["home", "about", "services", "blog", "contact"].map((item, index) => (
-        // eslint-disable-next-line jsx-a11y/anchor-has-content
-        <a
-          href={`#${item}`}
-          key={item + index}
-          className="app__navigation-dot"
-          style={
-            active === item
-              ? { backgroundColor: "#030343" }
-              : { border: "1px solid #f6eaea" }
-          }
-        />
-      ))}
+      {["home", "about", "services", "contact"].map(
+        (
+          item,
+          index //"blog removed"
+        ) => (
+          // eslint-disable-next-line jsx-a11y/anchor-has-content
+          <a
+            href={`#${item}`}
+            key={item + index}
+            className="app__navigation-dot"
+            style={
+              active === item
+                ? { backgroundColor: "#030343" }
+                : { border: "1px solid #f6eaea" }
+            }
+          />
+        )
+      )}
     </div>
   );
 };
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,40 +1,25 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { images } from "../../constants";
 import "./Header.scss";
 
-const scaleVariants = {
-  whileInView: {
-    scale: [0, 1],
-    opacity: [0, 1],
-    transition: {
-      duration: 1,
-      ease: "easeInOut",
-    },
-  },
-};
-
 const Header = () => {
   return (
-    <div id="home" className="app__header app__flex">
+    <div id="home" className="app__header">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 1 }}
         className="app__header-info"
       >
         <div className="app__header-badge">
-          <div className="badge-cmp app__flex">
-            <span>👋</span>
-            <div style={{ marginLeft: 20 }}>
+          <div className="badge-cmp">
+            <div>
               <p className="p-text">Hello, I am</p>
               <h1 className="head-text">Ishani Patel</h1>
+              <p className="p-text">Naturopathic Doctor</p>
             </div>
-          </div>
-
-          <div className="tag-cmp app__flex">
-            <p className="p-text">Naturopathic Doctor</p>
           </div>
         </div>
       </motion.div>
@@ -45,30 +30,13 @@ const Header = () => {
         className="app__header-img"
       >
         <img src={images.portrait} alt="profile-bg" />
-        <motion.img
-          whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: "easeInOut" }}
-          src={images.circle}
-          alt="profile-circle"
-          className="overlay_circle"
-        />
-      </motion.div>
-
-      <motion.div
-        variants={scaleVariants}
-        whileInView={scaleVariants.whileInView}
-        className="app__header-circles"
-      >
-        {[images.leafHeart, images.eatRight, images.handHolding].map(
-          (circle, index) => (
-            <div className="circle-cmp app__flex" key={`cirle-${index}`}>
-              <img src={circle} alt="profile-bg" />
-            </div>
-          )
-        )}
       </motion.div>
     </div>
   );
 };
 
-export default AppWrap(Header, "home");
+export default AppWrap(
+  MotionWrap(Header, "app__header"),
+  "home",
+  "app__whitebg"
+);
